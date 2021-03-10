ae0f647f1183fb5a3eaeb770d3aac177e2dd9186
smoganti <vamsee1993@gmail.com>
2021-03-10 16:14:54 -0700

    RPN solution

--- RPN_Stacks/src/com/stacksimpl/part1/InfixToPostFix.java
+++ RPN_Stacks/src/com/stacksimpl/part1/InfixToPostFix.java
@@ -19,16 +19,29 @@ public class InfixToPostFix {
     }
 
     static String infixToPostFix(String expression){
-
         String result = "";
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i <expression.length() ; i++)
-        {
+        for (int i = 0; i <expression.length() ; i++) {
+            char c = expression.charAt(i);
 
-            //TODO - Your CODE goes HERE
-            /**
-             *  Code
-             */
+            //check if char is operator
+            if(precedence(c)>0){
+                while(stack.isEmpty()==false && precedence(stack.peek())>=precedence(c)){
+                    result += stack.pop();
+                }
+                stack.push(c);
+            }else if(c==')'){
+                char x = stack.pop();
+                while(x!='('){
+                    result += x;
+                    x = stack.pop();
+                }
+            }else if(c=='('){
+                stack.push(c);
+            }else{
+                //character is neither operator nor (
+                result += c;
+            }
         }
         for (int i = 0; i <=stack.size() ; i++) {
             result += stack.pop();
--- RPN_Stacks/src/com/stacksimpl/part2/RPNEvaluation.java
+++ RPN_Stacks/src/com/stacksimpl/part2/RPNEvaluation.java
@@ -24,10 +24,79 @@ class solution {
         // in the array of the string
         for (int i = 0; i < tokens.length; i++) {
 
-            //TODO - Your CODE goes HERE
-            /**
-             *  Code
-             */
+
+            // If the character is not the special character
+            // ('+', '-' ,'*' , '/')
+            // then push the character to the stack
+            if (tokens[i] != "+" && tokens[i] != "-"
+                    && tokens[i] != "*" && tokens[i] != "/") {
+                stack.push(tokens[i]);
+                continue;
+            }
+            else {
+                // else if the character is the special
+                // character then use the switch method to
+                // perform the action
+                choice = tokens[i];
+            }
+
+            // Switch-Case
+            switch (choice) {
+                case "+":
+
+                    // Performing the "+" operation by poping
+                    // put the first two character
+                    // and then again store back to the stack
+
+                    x = Integer.parseInt(stack.pop());
+                    y = Integer.parseInt(stack.pop());
+                    value = x + y;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                case "-":
+
+                    // Performing the "-" operation by poping
+                    // put the first two character
+                    // and then again store back to the stack
+                    x = Integer.parseInt(stack.pop());
+                    y = Integer.parseInt(stack.pop());
+                    value = x - y;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                case "*":
+
+                    // Performing the "*" operation
+                    // by poping put the first two character
+                    // and then again store back to the stack
+
+                    x = Integer.parseInt(stack.pop());
+                    y = Integer.parseInt(stack.pop());
+                    value = x * y;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                case "/":
+
+                    // Performing the "/" operation by poping
+                    // put the first two character
+                    // and then again store back to the stack
+
+                    x = Integer.parseInt(stack.pop());
+                    y = Integer.parseInt(stack.pop());
+                    value = y / x;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                default:
+                    continue;
+            }
+
         }
 
         // Method to convert the String to integer
